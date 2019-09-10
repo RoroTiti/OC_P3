8ae7c60b3095d628a1147c35843839585792a813
Romaric Thibault <torburma@gmail.com>
2019-09-10 11:21:04 +0200

    Improved code organization + added items to collect random placement

--- main.py
+++ main.py
@@ -1,102 +1,50 @@
-from random import shuffle
 import curses
-
-
-def make_maze(w=7, h=7):
-    visited = [[False] * w + [True] for _ in range(h)] + [[True] * (w + True)]
-    vertical = [["# "] * w + ['#'] for _ in range(h)] + [[]]
-    horizontal = [["##"] * w + ['#'] for _ in range(h + 1)]
-
-    def walk(board_x, board_y):
-        visited[board_y][board_x] = True
-
-        directions = [(board_x - 1, board_y), (board_x, board_y + 1), (board_x + 1, board_y), (board_x, board_y - 1)]
-        shuffle(directions)
-        for (direction_x, direction_y) in directions:
-            if visited[direction_y][direction_x]:
-                continue
-            if direction_x == board_x:
-                horizontal[max(board_y, direction_y)][board_x] = "# "
-            if direction_y == board_y:
-                vertical[board_y][max(board_x, direction_x)] = "  "
-            walk(direction_x, direction_y)
-
-    # walk(randrange(w), randrange(h))
-    walk(0, 0)
-
-    string = ""
-    for (a, b) in zip(horizontal, vertical):
-        if a:
-            string += ''.join(a + ['\n'])
-        if b:
-            string += ''.join(b + ['\n'])
-
-    string = string[:- 1]
-
-    lines = string.split('\n')
-
-    maze = [['' for _ in range(len(lines[0]) + 2)] for _ in range(len(lines))]
-
-    curr_y = 1
-
-    for line in lines:
-        maze[curr_y - 1] = list(line)
-        curr_y += 1
-
-    # making the maze input and output
-    maze[1][0] = ' '
-    maze[len(lines[0]) - 2][len(lines) - 1] = ' '
-
-    return maze
+from models.maze import Maze
 
 
 def main(screen):
-    maze = make_maze()
+    maze_obj = Maze(15, 15)
+    maze_obj.generate_board()
+
+    maze_obj.place_items()
 
     # turn off cursor blinking
     curses.curs_set(0)
 
-    for x in range(len(maze)):
-        for y in range(len(maze[x])):
-            screen.addstr(y, x, maze[y][x])
-
-    # X / Y position
-    dot_pos = (0, 1)
-
-    screen.addstr(dot_pos[1], dot_pos[0], '.')
-
-    screen.refresh()
+    render_maze(screen, maze_obj)
 
     while 1:
         key = screen.getch()
 
         if key == curses.KEY_UP:
-            if not maze[dot_pos[1] - 1][dot_pos[0]] == '#':
-                screen.addstr(dot_pos[1], dot_pos[0], ' ')
-                dot_pos = (dot_pos[0], dot_pos[1] - 1)
-                screen.addstr(dot_pos[1], dot_pos[0], '.')
+            maze_obj.move_mg(maze_obj.DIRECTION_UP)
+            render_maze(screen, maze_obj)
 
         elif key == curses.KEY_DOWN:
-            if not maze[dot_pos[1] + 1][dot_pos[0]] == '#':
-                screen.addstr(dot_pos[1], dot_pos[0], ' ')
-                dot_pos = (dot_pos[0], dot_pos[1] + 1)
-                screen.addstr(dot_pos[1], dot_pos[0], '.')
+            maze_obj.move_mg(maze_obj.DIRECTION_DOWN)
+            render_maze(screen, maze_obj)
 
         elif key == curses.KEY_LEFT:
-            if not maze[dot_pos[1]][dot_pos[0] - 1] == '#':
-                screen.addstr(dot_pos[1], dot_pos[0], ' ')
-                dot_pos = (dot_pos[0] - 1, dot_pos[1])
-                screen.addstr(dot_pos[1], dot_pos[0], '.')
+            maze_obj.move_mg(maze_obj.DIRECTION_LEFT)
+            render_maze(screen, maze_obj)
 
         elif key == curses.KEY_RIGHT:
-            if not maze[dot_pos[1]][dot_pos[0] + 1] == '#':
-                screen.addstr(dot_pos[1], dot_pos[0], ' ')
-                dot_pos = (dot_pos[0] + 1, dot_pos[1])
-                screen.addstr(dot_pos[1], dot_pos[0], '.')
+            maze_obj.move_mg(maze_obj.DIRECTION_RIGHT)
+            render_maze(screen, maze_obj)
 
         elif key == curses.KEY_ENTER or key in [10, 13]:
-            # if user selected last row, exit the program
+            # if user press enter, exit the program
             exit()
+
+
+def render_maze(screen, maze: Maze):
+    for x in range(len(maze.board)):
+        for y in range(len(maze.board[x])):
+            screen.addstr(y, x, maze.board[y][x])
+
+    screen.addstr(maze.get_mg_xy_position()[1], maze.get_mg_xy_position()[0], '.')
+    screen.addstr(maze.get_a_xy_position()[1], maze.get_a_xy_position()[0], 'A')
+    screen.addstr(maze.get_b_xy_position()[1], maze.get_b_xy_position()[0], 'B')
 
 
 if __name__ == '__main__':
--- models/maze.py
+++ models/maze.py
@@ -1,3 +1,108 @@
+from random import shuffle, randint
+from typing import List
+
+
 class Maze:
-    def __init__(self):
-        pass
+    DIRECTION_UP = 1
+    DIRECTION_RIGHT = 2
+    DIRECTION_DOWN = 3
+    DIRECTION_LEFT = 4
+
+    def __init__(self, width: int, height: int):
+        self.width: int = width
+        self.height: int = height
+        self.board = [['' for _ in range(self.width)] for _ in range(self.height)]
+        self.__mg_xy_position = (0, 1)
+        self.__a_xy_position = (0, 0)
+        self.__b_xy_position = (0, 0)
+
+    def generate_board(self):
+        w: int = int((self.width - 1) / 2)
+        h: int = int((self.height - 1) / 2)
+
+        visited = [[False] * w + [True] for _ in range(h)] + [[True] * (w + True)]
+        vertical = [["# "] * w + ['#'] for _ in range(h)] + [[]]
+        horizontal = [["##"] * w + ['#'] for _ in range(h + 1)]
+
+        def walk(board_x, board_y):
+            visited[board_y][board_x] = True
+
+            directions = [
+                (board_x - 1, board_y),
+                (board_x, board_y + 1),
+                (board_x + 1, board_y),
+                (board_x, board_y - 1)
+            ]
+
+            shuffle(directions)
+            for (direction_x, direction_y) in directions:
+                if visited[direction_y][direction_x]:
+                    continue
+                if direction_x == board_x:
+                    horizontal[max(board_y, direction_y)][board_x] = "# "
+                if direction_y == board_y:
+                    vertical[board_y][max(board_x, direction_x)] = "  "
+                walk(direction_x, direction_y)
+
+        walk(0, 0)
+
+        curr_y = 1
+
+        for (a, b) in zip(horizontal, vertical):
+            if a:
+                self.board[curr_y - 1] = list(''.join(a))
+                curr_y += 1
+            if b:
+                self.board[curr_y - 1] = list(''.join(b))
+                curr_y += 1
+
+        # making the maze input and output
+        self.board[1][0] = ' '
+        self.board[self.height - 2][self.width - 1] = ' '
+
+    def place_items(self):
+        # Step 0 : init
+        free_blocks_xy_positions: List[(int, int)] = []
+
+        # Step 1 : getting all empty blocks of board
+        for y in range(len(self.board)):
+            for x in range(len(self.board[y])):
+                if self.board[y][x] == ' ':
+                    free_blocks_xy_positions.append((y, x))
+
+        # Shuffle the array to mix the positions
+        shuffle(free_blocks_xy_positions)
+
+        # Step 2 : choosing place of element A
+        element_a_index = randint(0, len(free_blocks_xy_positions) // 2)
+        self.__a_xy_position = free_blocks_xy_positions[element_a_index]
+
+        # Step 3 : choosing place of element B
+        element_b_index = randint(len(free_blocks_xy_positions) // 2, len(free_blocks_xy_positions) - 1)
+        self.__b_xy_position = free_blocks_xy_positions[element_b_index]
+
+    def move_mg(self, direction: int):
+        if direction == self.DIRECTION_UP:
+            if not self.board[self.__mg_xy_position[1] - 1][self.__mg_xy_position[0]] == '#':
+                self.__mg_xy_position = (self.__mg_xy_position[0], self.__mg_xy_position[1] - 1)
+
+        elif direction == self.DIRECTION_RIGHT:
+            if not self.board[self.__mg_xy_position[1]][self.__mg_xy_position[0] + 1] == '#':
+                self.__mg_xy_position = (self.__mg_xy_position[0] + 1, self.__mg_xy_position[1])
+
+        elif direction == self.DIRECTION_DOWN:
+            if not self.board[self.__mg_xy_position[1] + 1][self.__mg_xy_position[0]] == '#':
+                self.__mg_xy_position = (self.__mg_xy_position[0], self.__mg_xy_position[1] + 1)
+
+        elif direction == self.DIRECTION_LEFT:
+            if not self.board[self.__mg_xy_position[1]][self.__mg_xy_position[0] - 1] == '#':
+                self.__mg_xy_position = (self.__mg_xy_position[0] - 1, self.__mg_xy_position[1])
+
+    def get_mg_xy_position(self) -> (int, int):
+        return self.__mg_xy_position
+
+    def get_a_xy_position(self) -> (int, int):
+        return self.__a_xy_position
+
+    def get_b_xy_position(self) -> (int, int):
+        return self.__b_xy_position
